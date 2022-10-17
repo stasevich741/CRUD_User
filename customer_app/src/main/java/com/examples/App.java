de1437859131a83a3ca4ce391f9831876eddc5bd
Stanislav Korolkov <stasevich741@gmail.com>
2022-10-17 23:53:11 +0300

    logging

--- customer_app/src/main/java/com/examples/App.java
+++ customer_app/src/main/java/com/examples/App.java
@@ -1,13 +1,21 @@
 package com.examples;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@Slf4j
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
     }
 }
--- customer_app/src/main/java/com/examples/service/CustomerService.java
+++ customer_app/src/main/java/com/examples/service/CustomerService.java
@@ -3,11 +3,13 @@ package com.examples.service;
 import com.examples.entity.Customer;
 import com.examples.exception.NotFoundException;
 import com.examples.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class CustomerService {
 
     private CustomerRepository repository;
@@ -22,6 +24,10 @@ public class CustomerService {
 
     public Customer getCustomerById(Long id) {
         return repository.findById(id)
-                .orElseThrow(() -> new NotFoundException("customer with id " + id + " not found"));
+                .orElseThrow(() -> {
+                    NotFoundException notFoundException = new NotFoundException("customer with id " + id + " not found");
+                    log.error("error in getCustomerById for id : {} ", id, notFoundException);
+                    return notFoundException;
+                });
     }
 }
