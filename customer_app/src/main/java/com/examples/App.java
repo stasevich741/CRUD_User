47ab8693dcfefb66165a35b743bb683fc8ae6f96
Stanislav Korolkov <stasevich741@gmail.com>
2022-10-18 14:30:53 +0300

    the end

--- customer_app/src/main/java/com/examples/App.java
+++ customer_app/src/main/java/com/examples/App.java
@@ -11,11 +11,5 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-
-        log.trace("A TRACE Message");
-        log.debug("A DEBUG Message");
-        log.info("An INFO Message");
-        log.warn("A WARN Message");
-        log.error("An ERROR Message");
     }
 }
